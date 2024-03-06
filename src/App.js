4fd2737678a331f8b083844a4ab94ad3a861d283
Thaís Costa <40796612+costathais@users.noreply.github.com>
2024-03-06 19:18:45 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        Hello World
+        <h2></h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
